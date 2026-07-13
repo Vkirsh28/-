f832283ce988eec616c2d8918e3039f0b2db73c5
vl.meshcheryakov <vl.meshcheryakov28@icloud.com>
2026-07-13 20:36:34 +0300

    Переменные + int

---
+++ src/main/Java/dva.java
@@ -0,0 +1,10 @@
+public class dva {
+    public static void main(String[] args)
+    {
+        System.out.println("Привет" + "Пока" + "28деняг дай" );
+
+    int l = 6;
+    int s = l * l;
+        System.out.println("Переменные" + "Площадть квадрата со стороной "  +  l + " = " + s );
+    }
+}
