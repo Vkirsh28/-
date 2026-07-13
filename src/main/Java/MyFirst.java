e84537a3a49c8b668bf8007313ae5a94a3604500
vl.meshcheryakov <vl.meshcheryakov28@icloud.com>
2026-07-13 20:02:12 +0300

    Sout + счет

--- src/main/Java/MyFirst.java
+++ src/main/Java/MyFirst.java
@@ -1,5 +1,20 @@
 public class MyFirst {
+
     public static void main(String[] args) {
         System.out.println("Здарова заебал!");
+
+        System.out.println(2 + 2);
+        System.out.println(2 / 2);
+        System.out.println(2 * 2);
+        System.out.println(2 / 2);
+        System.out.println(1 / 2);
+        System.out.println(1.0 / 2);
+        System.out.println(2.0 / 2);
+        System.out.println(2 / 2.0);
+        System.out.println("2" + "2");
+        System.out.println("2" + 2);
+        System.out.println("2 + 2 =" + (2 + 2));
+
     }
 }
+
