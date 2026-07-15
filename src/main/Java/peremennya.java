0203bd21e53ec3066d4701ddf4000b6bdb7aecde
vl.meshcheryakov <vl.meshcheryakov28@icloud.com>
2026-07-15 16:39:30 +0300

    Переменные + DOUBLE для значений с плавающей точкой и somebody

---
+++ src/main/Java/peremennya.java
@@ -0,0 +1,9 @@
+public class peremennya {
+    public static void main(String[] args) {
+        String somebody = "Double использется для значений с плавающей точкой";
+        System.out.println("Переменная " + somebody + "!" );
+        double l = 6.0;
+      double s = l * l;
+        System.out.println("Переменные" + " Площадть квадрата со стороной "  +  l + " = " + s );
+    }
+}
